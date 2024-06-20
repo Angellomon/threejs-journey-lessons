cb39ebd8e548f202319f28f86f8c9562fc65ea09
Angellomon <angelmtzdiaz@gmail.com>
2024-06-20 17:27:03 -0600

    feat(menu): impl separator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,21 @@
 			<Menubar.Content>
 				<a href="/lesson-03"><Menubar.Item>03 - Geometry</Menubar.Item></a>
 				<Menubar.Separator />
+
 				<a href="/lesson-04"><Menubar.Item>04 - Transform Objects</Menubar.Item></a>
+				<Menubar.Separator />
+
 				<a href="/lesson-05"><Menubar.Item>05 - Animation</Menubar.Item></a>
+				<Menubar.Separator />
+
 				<a href="/lesson-06"><Menubar.Item>06 - Cameras</Menubar.Item></a>
+				<Menubar.Separator />
+
 				<a href="/lesson-07"><Menubar.Item>07 - Fullscreen and resizing</Menubar.Item></a>
+				<Menubar.Separator />
+
 				<a href="/lesson-08"><Menubar.Item>08 - Geometries</Menubar.Item></a>
+				<Menubar.Separator />
 			</Menubar.Content>
 		</Menubar.Menu>
 	</Menubar.Root>
