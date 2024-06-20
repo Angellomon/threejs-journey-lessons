c87de324eb6667eac6d6c1b2a7e374a75be91e0c
Angellomon <angelmtzdiaz@gmail.com>
2024-06-20 01:59:03 -0600

    feat(lessons): impl lesson 07 - Fullscreen & resizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,19 @@
 	import '../app.css';
 </script>
 
-<Menubar.Root>
-	<Menubar.Menu>
-		<Menubar.Trigger>Lessons</Menubar.Trigger>
-		<Menubar.Content>
-			<a href="/lesson-03"><Menubar.Item>03 - Geometry</Menubar.Item></a>
-			<Menubar.Separator />
-			<a href="/lesson-04"><Menubar.Item>04 - Transform Objects</Menubar.Item></a>
-			<a href="/lesson-05"><Menubar.Item>05 - Animation</Menubar.Item></a>
-			<a href="/lesson-06"><Menubar.Item>06 - Cameras</Menubar.Item></a>
-		</Menubar.Content>
-	</Menubar.Menu>
-</Menubar.Root>
-
+<div class="menu">
+	<Menubar.Root>
+		<Menubar.Menu>
+			<Menubar.Trigger>Lessons</Menubar.Trigger>
+			<Menubar.Content>
+				<a href="/lesson-03"><Menubar.Item>03 - Geometry</Menubar.Item></a>
+				<Menubar.Separator />
+				<a href="/lesson-04"><Menubar.Item>04 - Transform Objects</Menubar.Item></a>
+				<a href="/lesson-05"><Menubar.Item>05 - Animation</Menubar.Item></a>
+				<a href="/lesson-06"><Menubar.Item>06 - Cameras</Menubar.Item></a>
+				<a href="/lesson-07"><Menubar.Item>07 - Fullscreen and resizing</Menubar.Item></a>
+			</Menubar.Content>
+		</Menubar.Menu>
+	</Menubar.Root>
+</div>
 <slot />
---
+++ src/routes/lesson-07/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+	import { browser } from '$app/environment';
+	import { createMeshBasicCube, newSizes } from '$lib/utils';
+	import { onMount } from 'svelte';
+	import * as THREE from 'three';
+	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+	const DEFAULT_FOV = 75;
+
+	/**@type {HTMLCanvasElement}*/
+	let canvas;
+
+	if (browser) {
+		const menuHeight = document.querySelector('.menu')?.clientHeight || 0;
+
+		function setupScene() {
+			const scene = new THREE.Scene();
+
+			const cubesGroup = new THREE.Group();
+
+			scene.add(cubesGroup);
+
+			const redCube = createMeshBasicCube(1, 0xff0000);
+			cubesGroup.add(redCube);
+
+			const cameraSizes = newSizes(window.innerWidth, window.innerHeight - menuHeight);
+
+			const camera = new THREE.PerspectiveCamera(DEFAULT_FOV, cameraSizes.aspect());
+
+			const controls = new OrbitControls(camera, canvas);
+
+			camera.position.z = 5;
+
+			const renderer = new THREE.WebGLRenderer({
+				canvas
+			});
+
+			renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+			renderer.setSize(cameraSizes.width, cameraSizes.height);
+
+			function tick() {
+				renderer.render(scene, camera);
+
+				controls.update();
+
+				window.requestAnimationFrame(tick);
+			}
+
+			window.requestAnimationFrame(tick);
+			window.addEventListener('resize', () => {
+				// @ts-ignore
+				const isFullscreen = document.fullscreenElement || document.webkitFullscreenElement;
+				cameraSizes.width = window.innerWidth;
+				if (!isFullscreen) cameraSizes.height = window.innerHeight - menuHeight;
+				else cameraSizes.height = window.innerHeight;
+
+				camera.aspect = cameraSizes.aspect();
+				camera.updateProjectionMatrix();
+
+				renderer.setSize(cameraSizes.width, cameraSizes.height);
+			});
+			window.addEventListener('dblclick', () => {
+				// @ts-ignore
+				const isFullscreen = document.fullscreenElement || document.webkitFullscreenElement;
+
+				if (!isFullscreen) canvas.requestFullscreen();
+				else document.exitFullscreen();
+			});
+		}
+
+		onMount(() => {
+			setupScene();
+		});
+	}
+</script>
+
+<canvas bind:this={canvas}></canvas>
