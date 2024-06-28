aa8d5efdad62ab940a50fb9d085d74b40970fc49
Angellomon <angelmtzdiaz@gmail.com>
2024-06-27 19:32:52 -0600

    feat(lessons/12-3d-text): hmmm

--- src/routes/lesson-12/+page.svelte
+++ src/routes/lesson-12/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+	import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+	import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
 	import { createMeshBasicCube, newSizes } from '$lib/utils';
@@ -14,8 +16,32 @@
 		function setupScene() {
 			const scene = new THREE.Scene();
 
-			const cube = createMeshBasicCube(1, 0xffffff);
-			scene.add(cube);
+			const fontLoader = new FontLoader();
+			fontLoader.load('/fonts/mulish.typeface.json', (font) => {
+				console.log('font loaded');
+
+				const textMesh = new THREE.Mesh(
+					new TextGeometry('hmmm', {
+						font,
+						size: 0.5,
+						height: 0.2,
+						curveSegments: 12,
+						bevelEnabled: true,
+						bevelThickness: 0.03,
+						bevelSize: 0.02,
+						bevelOffset: 0,
+						bevelSegments: 5
+					}),
+					new THREE.MeshBasicMaterial({
+						color: 0xff0000
+					})
+				);
+
+				scene.add(textMesh);
+			});
+
+			// const cube = createMeshBasicCube(1, 0xffffff);
+			// scene.add(cube);
 
 			const cameraSizes = newSizes(window.innerWidth, window.innerHeight - menuHeight);
 
